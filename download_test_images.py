8525511ee97d4a556d3a242dc8e220394e450a75
Siddarth Nanduri <125226780+SiddarthNanduri@users.noreply.github.com>
2025-04-15 14:10:06 -0400

    Update download_test_images.py

--- download_test_images.py
+++ download_test_images.py
@@ -8,13 +8,13 @@ def download_test_images(num_images=5, output_dir='test_images'):
     # Create output directory
     os.makedirs(output_dir, exist_ok=True)
     
-    # COCO validation images URLs (these are public domain images)
+    # COCO validation images URLs 
     image_urls = [
-        'http://images.cocodataset.org/val2017/000000397133.jpg',  # A person riding a horse
-        'http://images.cocodataset.org/val2017/000000037777.jpg',  # A dining table with food
-        'http://images.cocodataset.org/val2017/000000252219.jpg',  # A city street scene
-        'http://images.cocodataset.org/val2017/000000087038.jpg',  # A beach scene
-        'http://images.cocodataset.org/val2017/000000174482.jpg'   # A group of people
+        'http://images.cocodataset.org/val2017/000000397133.jpg',  
+        'http://images.cocodataset.org/val2017/000000037777.jpg',  
+        'http://images.cocodataset.org/val2017/000000252219.jpg',  
+        'http://images.cocodataset.org/val2017/000000087038.jpg',  
+        'http://images.cocodataset.org/val2017/000000174482.jpg'   
     ]
     
     print(f"Downloading {num_images} test images to {output_dir}...")
